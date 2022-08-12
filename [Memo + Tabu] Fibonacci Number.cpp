904f6158e6bedb2a785a8fcff3d3f2d67d17372c
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-12 16:39:26 +0530

    Update and rename Fibonacci Number.cpp to [Memo + Tabu] Fibonacci Number.cpp

--- [Memo + Tabu] Fibonacci Number.cpp
+++ [Memo + Tabu] Fibonacci Number.cpp
@@ -1,3 +1,4 @@
+// memorization
 class Solution {
 public:
     int solve(int n, vector<int> &dp){
@@ -15,5 +16,23 @@ public:
     int fib(int n) {
         vector<int> dp(n+1,-1);
         return solve(n,dp);
+    }
+};
+
+// Tabulation
+class Solution {
+public:
+    int fib(int n) {
+        int prev=1;
+        int prev2=0;
+        if(n==0)
+            return 0;
+            
+        for(int i=2;i<=n;i++){
+            int curr=prev+prev2;
+            prev2=prev;
+            prev=curr;
+        }
+        return prev;
     }
 };
