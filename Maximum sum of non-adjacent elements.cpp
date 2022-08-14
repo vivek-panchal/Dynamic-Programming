8668c6861d6f77bd08af8bf8fc7869d303f76a25
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-14 14:23:45 +0530

    Create Maximum sum of non-adjacent elements.cpp

---
+++ Maximum sum of non-adjacent elements.cpp
@@ -0,0 +1,65 @@
+//Recursive 
+int solve(int index,vector<int> &nums){
+    if(index==0)
+        return nums[0];
+    if(index<0)
+        return 0;
+    int pick=nums[index]+solve(index-2,nums);
+    int dontPick=0+ solve(index-1,nums);
+    return max(pick,dontPick);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n= nums.size();
+    return solve(n-1,nums);
+}
+// memoization
+int solve(int index,vector<int> &nums,vector<int>&dp){
+    if(index==0)
+        return nums[0];
+    if(index<0)
+        return 0;
+    if(dp[index]!=-1)
+        return dp[index];
+    int pick=nums[index]+solve(index-2,nums,dp);
+    int dontPick=0+ solve(index-1,nums,dp);
+    return dp[index]=max(pick,dontPick);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n= nums.size();
+    vector<int> dp(n,-1);
+    return solve(n-1,nums,dp);
+}
+
+// tabulation 
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n= nums.size();
+    vector<int> dp(n,-1);
+     dp[0]=nums[0];
+    for(int i=1;i<n;i++){
+        int pick=nums[i];
+        if(i>1)
+            pick+=dp[i-2];
+        int dontPick=0+dp[i-1];
+        dp[i]=max(pick,dontPick);
+    }
+    return (dp[n-1]);
+}
+
+// space optemization
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    int n= nums.size();
+    vector<int> dp(n,-1);
+     int prev=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int pick=nums[i];
+        if(i>1)
+            pick+=prev2;
+        int dontPick=0+prev;
+        int curr=max(pick,dontPick);
+        prev2=prev;
+        prev=curr;
+    }
+    return prev;
+}
