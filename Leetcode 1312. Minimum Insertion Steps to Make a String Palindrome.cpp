183f827129b3b05a02b741688892bffb0e3fbd41
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-21 18:21:24 +0530

    Create Leetcode 1312. Minimum Insertion Steps to Make a String Palindrome.cpp

---
+++ Leetcode 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,29 @@
+// find longest common palindrome substract it from size given string 
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n= text1.size();
+        int m=text2.size();
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+        
+        for(int j=0;j<=m;j++)
+            prev[j]=0;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1])
+                     curr[j]= 1 + prev[j-1];
+                else
+                     curr[j]=max(prev[j],curr[j-1]);
+            }
+            prev=curr;
+        }
+        return prev[m];
+    }
+    int minInsertions(string s) {
+        string t=s;
+        reverse(t.begin(),t.end());
+        return s.size() - longestCommonSubsequence(s,t);
+    }
+};
