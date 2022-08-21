bf0729b26fe80f118b008c5bab20157006041c6b
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-21 18:01:13 +0530

    Update Longest Common Substring.cpp

--- Longest Common Substring.cpp
+++ Longest Common Substring.cpp
@@ -1,4 +1,6 @@
 // same as  longest common subsequence
+
+//Tabulation
 int lcs(string &text1, string &text2){
 	int n= text1.size();
         int m=text2.size();
@@ -17,6 +19,28 @@ int lcs(string &text1, string &text2){
                 else
                      dp[i][j]=0;
             }
+        }
+    return ans;
+}
+
+// Space optemize
+int lcs(string &text1, string &text2){
+        int n= text1.size();
+        int m=text2.size();
+        int ans=0;
+        vector<int> prev(m+1,0);
+        vector<int> curr(m+1,0);
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(text1[i-1]==text2[j-1]){
+                    curr[j]= 1 + prev[j-1];
+                    ans=max(ans,curr[j]);
+                }
+                else
+                     curr[j]=0;
+            }
+            prev=curr;
         }
     return ans;
 }
