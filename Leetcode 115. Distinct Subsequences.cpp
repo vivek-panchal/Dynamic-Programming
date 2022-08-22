bf75d36e7fd55b31e765951cb046591753f1a7bd
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-22 18:46:32 +0530

    Update Leetcode 115. Distinct Subsequences.cpp

--- Leetcode 115. Distinct Subsequences.cpp
+++ Leetcode 115. Distinct Subsequences.cpp
@@ -69,3 +69,30 @@ public:
     }
 };
 ```
+//space Optemization
+```
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        int n=s.size();
+        int m=t.size();
+        vector<double> prev(m+1,0);
+        vector<double> curr(m+1,0);
+        
+        curr[0]=1;
+        prev[0]=1;
+        
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s[i-1]==t[j-1]){
+                     curr[j]= prev[j-1] + prev[j];
+                }
+                else
+                 curr[j]=prev[j];
+            }
+            prev=curr;
+        }
+        return (int)prev[m];
+    }
+};
+```
