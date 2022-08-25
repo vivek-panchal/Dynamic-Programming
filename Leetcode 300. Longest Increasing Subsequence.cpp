ae3e2ceb5b7b0521f8540cdf6d204bee017d1806
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-25 18:48:46 +0530

    Update Leetcode 300. Longest Increasing Subsequence.cpp

--- Leetcode 300. Longest Increasing Subsequence.cpp
+++ Leetcode 300. Longest Increasing Subsequence.cpp
@@ -85,3 +85,26 @@ public:
     }
 };
 ```
+
+//Using Binary Search
+```
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        vector<int> ans;
+        int length=1;
+        ans.push_back(nums[0]);
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]>ans.back()){
+                ans.push_back(nums[i]);
+                length++;
+            }
+            else{
+                int index=lower_bound(ans.begin(),ans.end(),nums[i])-ans.begin();
+                ans[index]=nums[i];
+            }
+        }
+        return length;
+    }
+};
+```
