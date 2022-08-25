99db0f763d9bd614059dd4abf2d223bcb46c2d62
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-25 18:30:08 +0530

    Update Leetcode 300. Longest Increasing Subsequence.cpp

--- Leetcode 300. Longest Increasing Subsequence.cpp
+++ Leetcode 300. Longest Increasing Subsequence.cpp
@@ -41,3 +41,47 @@ public:
     }
 };
 ```
+//Tabulation
+```
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(n+1,0));
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int notTake=0+ dp[ind+1][prev+1];
+                int take=0;
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    take=1+dp[ind+1][ind+1];
+                }
+                 dp[ind][prev+1]= max(take,notTake);
+            }
+        }
+        return dp[0][0];
+    }
+};
+```
+
+//Space optemize
+```
+class Solution {
+public:
+    int lengthOfLIS(vector<int>& nums) {
+        int n=nums.size();
+        vector<int> next(n+1,0),curr(n+1,0);
+        for(int ind=n-1;ind>=0;ind--){
+            for(int prev=ind-1;prev>=-1;prev--){
+                int notTake=0+ next[prev+1];
+                int take=0;
+                if(prev==-1 || nums[ind]>nums[prev]){
+                    take=1+next[ind+1];
+                }
+                 curr[prev+1]= max(take,notTake);
+            }
+            next=curr;
+        }
+        return next[0];
+    }
+};
+```
