3d6347a4c1ab2220917644d5d55021f03aecd4ab
vivek <95127627+vivek-panchal@users.noreply.github.com>
2022-08-27 13:58:26 +0530

    Create Longest Bitonic Sequence.cpp

---
+++ Longest Bitonic Sequence.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+int longestBitonicSequence(vector<int>& arr, int n) {
+    vector<int> dp1(n,1);
+    vector<int> dp2(n,1);
+    
+    for(int i=0;i<n;i++){
+        for(int prev=0;prev<i;prev++){
+            if(arr[prev]<arr[i] && 1+ dp1[prev] > dp1[i])
+                dp1[i]=1+dp1[prev];
+        }
+    }
+    
+    for(int i=n-1;i>=0;i--){
+        for(int prev=n-1;prev>i;prev--){
+            if(arr[prev]<arr[i] && 1+ dp2[prev] > dp2[i])
+                dp2[i]=1+dp2[prev];
+        }
+    }
+    
+    int maxi=0;
+    for(int i=0;i<n;i++){
+        maxi=max(maxi,dp1[i]+dp2[i]-1);
+    }
+    return maxi;
+} 
