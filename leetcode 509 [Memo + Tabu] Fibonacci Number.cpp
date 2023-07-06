6469ed7eb6c8410650d6285e176f20e065d7c798
Vivek Panchal <95127627+vivek-panchal@users.noreply.github.com>
2023-07-06 11:32:19 +0530

    Update leetcode 509 [Memo + Tabu] Fibonacci Number.cpp

--- leetcode 509 [Memo + Tabu] Fibonacci Number.cpp
+++ leetcode 509 [Memo + Tabu] Fibonacci Number.cpp
@@ -19,7 +19,24 @@ public:
     }
 };
 
-// Tabulation
+//Tabulation
+class Solution {
+public:
+    int fib(int n) {
+        if(n<=1){
+            return n;
+        }
+        vector<int> dp(n+1,-1);
+        dp[0]=0;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+        }
+        return dp[n];
+    }
+};
+
+// Space optemization
 class Solution {
 public:
     int fib(int n) {
